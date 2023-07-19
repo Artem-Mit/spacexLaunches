a22cc996b7aa1f26b429587eac928ecfd455ea65
Artem <artem.v.mitin@yandex.ru>
2023-07-19 20:32:28 +0300

    fix buttons and labels

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -9,6 +9,7 @@ interface IProps {
 export default function Filter({ onMaxDate, onMinDate }: IProps) {
   return (
     <div className="app-container__filters">
+      <span className="app-container__sort-text">Sort date:</span>
       <div className="app-container__sort-button app-container__sort-button_up" onClick={onMaxDate} aria-hidden />
       <div className="app-container__sort-button app-container__sort-button_bottom" onClick={onMinDate} aria-hidden />
     </div>
